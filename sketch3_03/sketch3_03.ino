15bf021901d622b001d8fa9cfd1ae5d38c57be54
Do Tai <dotai.3373@gmail.com>
2021-01-28 10:40:07 +0700

    Blink Led and Increase delay time

---
+++ sketch3_03/sketch3_03.ino
@@ -0,0 +1,15 @@
+int ledPin = 13;
+int delayPeriod = 500;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
+  delay(delayPeriod);
+  digitalWrite(ledPin, LOW);
+  delay(delayPeriod);
+  delayPeriod += 100;
+}
